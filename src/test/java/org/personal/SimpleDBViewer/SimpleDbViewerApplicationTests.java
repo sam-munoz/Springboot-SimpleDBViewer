1ae743875d9abf98ab1d1ab80d4ecfa60c3aea27
sam-munoz <sammunoz2019@gmail.com>
2024-08-02 16:28:41 -0500

    first upload

---
+++ src/test/java/org/personal/SimpleDBViewer/SimpleDbViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.SimpleDBViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDbViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
